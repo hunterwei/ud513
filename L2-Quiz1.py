da147c59fcbfb25a07063c5c087dc7eb42bc9511
Hunter Wei <hunterwei@gmail.com>
2022-09-23 12:11:26 -0500

    Completed Lesson2 Quiz1

---
+++ L2-Quiz1.py
@@ -0,0 +1,105 @@
+"""The LinkedList code from before is provided below.
+Add three functions to the LinkedList.
+"get_position" returns the element at a certain position.
+The "insert" function will add an euument to a particular
+spot in the list.
+"delete" will delete the first element with that
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
+
+
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        element = None
+        for i in range(1, position + 1):
+            if i == 1:
+                element = self.head
+            else:
+                if element is not None:
+                    element = element.next
+                else:
+                    break
+
+        return element
+
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        target_element = self.get_position(position)
+        new_element.next = target_element
+
+        prev_element = self.get_position(position - 1)
+        if prev_element is not None:
+            prev_element.next = new_element
+
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        if value == self.head.value:
+            self.head = self.head.next
+            return
+
+        prev_element = self.head
+        while prev_element.next:
+            element = prev_element.next
+            if value == element.value:
+                prev_element.next = element.next
+                return
+            prev_element = element
+
+
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+
+# Test get_position
+# Should print 3
+print(ll.head.next.next.value)
+
+# Should also print 3
+print(ll.get_position(3).value)
+
+# Test insert
+ll.insert(e4, 3)
+# Should print 4 now
+print(ll.get_position(3).value)
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print(ll.get_position(1).value)
+# Should print 4 now
+print(ll.get_position(2).value)
+# Should print 3 now
+print(ll.get_position(3).value)
